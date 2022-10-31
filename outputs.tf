2601e9199fe4e52911e868256ddedc332f14a5d3
Premdeep Saini <premdeep.saini@vedantu.com>
2022-11-01 01:28:42 +0530

    feat: output the application target group arn

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "alb_https_listener_arn" {
   description = "ARN for the application load balancer HTTPS listener"
   value       = aws_lb_listener.app_https.arn
 }
+
+output "app_target_group_arn" {
+  description = "ARN for the application target group"
+  value       = aws_lb_target_group.app.arn
+}
